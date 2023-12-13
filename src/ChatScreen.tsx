ba5ff2b42fdcc24bf1b143cc3669bcbc09f235a5
Hardik Upadhyay <57345910+Hardik888@users.noreply.github.com>
2023-12-14 01:10:28 +0530

    Added Broadcast Message

--- src/ChatScreen.tsx
+++ src/ChatScreen.tsx
@@ -11,6 +11,7 @@ import {
   KeyboardAvoidingView,
 } from 'react-native';
 import {useAuth} from './authContext';
+import Message from './Message';
 
 interface UserData {
   id: number;
@@ -23,7 +24,9 @@ const ChatScreen: React.FC = () => {
   const [selectedUserEmail, setSelectedUserEmail] = useState<string | null>(
     null,
   );
+  const [nextscreen, setNextScreen] = useState(false); // Changed variable name
   const [messageText, setMessageText] = useState('');
+  const [broadcastMessage, setBroadcastMessage] = useState(''); // Added broadcastMessage state
   const [bitmojiMap, setBitmojiMap] = useState<{[email: string]: number}>({});
   const [loggedInUserId, setLoggedInUserId] = useState<number | undefined>(
     undefined,
@@ -49,10 +52,11 @@ const ChatScreen: React.FC = () => {
           const loggedInUserData = data.find(
             user => user.email === loggedInUserEmail,
           );
+          if (loggedInUserData) {
+            const {id} = loggedInUserData;
+            setLoggedInUserId(id);
+          }
 
-          const {id} = loggedInUserData;
-          setLoggedInUserId(id);
-          // Filter out the logged-in user's data
           setUserData(data.filter(user => user.email !== loggedInUserEmail));
         } else {
           console.error('Failed to fetch user data');
@@ -94,13 +98,18 @@ const ChatScreen: React.FC = () => {
             style={styles.messageInputContainer}>
             <TextInput
               style={styles.messageInput}
+              placeholder="Speed Post"
               value={messageText}
               onChangeText={text => setMessageText(text)}
             />
             <TouchableOpacity
               style={styles.sendButton}
-              onPress={() => sendMessage(loggedInUserId, item.id)}>
-              <Text>+</Text>
+              onPress={() => {
+                if (loggedInUserId !== undefined) {
+                  sendMessage(loggedInUserId, item.id);
+                }
+              }}>
+              <Text>🎯</Text>
             </TouchableOpacity>
           </KeyboardAvoidingView>
         )}
@@ -132,7 +141,6 @@ const ChatScreen: React.FC = () => {
 
       if (response.ok) {
         setMessageText('');
-        // You might want to update the UI or do something after a successful send
       } else {
         console.error('Failed to send message');
       }
@@ -141,21 +149,71 @@ const ChatScreen: React.FC = () => {
     }
   };
 
-  return (
-    <ImageBackground
-      source={require('../assets/background3.jpg')}
-      style={styles.backgroundImage}
-      resizeMode="cover">
-      <View style={styles.container}>
-        <Text style={styles.title}>Connect</Text>
-        <FlatList
-          data={userData}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={renderUserItem}
-        />
-      </View>
-    </ImageBackground>
-  );
+  const sendMessageToAll = async () => {
+    if (!broadcastMessage) {
+      return;
+    }
+
+    try {
+      const response = await fetch('http://10.0.2.2:80/myapp/broadcast.php', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          sender_id: loggedInUserId,
+          message_text: broadcastMessage,
+        }),
+      });
+
+      if (response.ok) {
+        setBroadcastMessage('');
+      } else {
+        console.error('Failed to send broadcast message');
+      }
+    } catch (error) {
+      console.error('Error sending broadcast message:', error);
+    }
+  };
+
+  if (!nextscreen) {
+    return (
+      <ImageBackground
+        source={require('../assets/background3.jpg')}
+        style={styles.backgroundImage}
+        resizeMode="cover">
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={styles.allchats}
+            onPress={() => setNextScreen(true)}>
+            <Text style={styles.title}>ALL CHATS 💬</Text>
+          </TouchableOpacity>
+          <FlatList
+            data={userData}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={renderUserItem}
+          />
+          {/* Broadcast feature */}
+          <View style={styles.broadcastContainer}>
+            <TextInput
+              style={styles.broadcastInput}
+              placeholder="Broadcast message to all users"
+              value={broadcastMessage}
+              onChangeText={text => setBroadcastMessage(text)}
+            />
+            <TouchableOpacity
+              style={styles.broadcastButton}
+              onPress={sendMessageToAll}>
+              <Text>📢</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  } else {
+    return <Message />;
+  }
 };
 
 const styles = StyleSheet.create({
@@ -167,13 +225,19 @@ const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
     width: '120%',
-    height: '200%',
+    height: '100%',
   },
   title: {
-    fontSize: 15,
+    fontSize: 16,
     fontWeight: 'bold',
-    marginBottom: 20,
-    color: 'rgba(20, 186, 280, 0.5)',
+    marginBottom: -20,
+
+    color: 'rgba(230, 180, 210, 1)',
+    backgroundColor: 'rgba(20, 186, 280, 0.5)',
+    borderRadius: 100,
+    height: '20%',
+    width: '30%',
+    textAlign: 'center',
   },
   userItem: {
     flexDirection: 'row',
@@ -203,22 +267,51 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 5,
+    width: '180%',
   },
   messageInput: {
-    flex: 1,
-    height: '50%',
-    borderColor: 'gray',
+    height: '55%',
+    borderColor: 'grey',
     borderWidth: 0.5,
     marginTop: 18,
-    paddingLeft: 2,
-    borderRadius: 10,
+    paddingLeft: 1,
+    borderRadius: 30,
   },
   sendButton: {
-    padding: 4,
+    height: '40%',
+    padding: 5,
+    backgroundColor: 'transparent',
+
+    marginTop: 15,
+  },
+  allchats: {
+    /* backgroundColor: 'rgba(136, 219, 188, 0.7) */
+  },
+  // Broadcast styles
+  broadcastContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  broadcastInput: {
+    height: 35, // Adjust the height as needed
+    borderColor: 'grey',
+    borderWidth: 0.5,
+    marginRight: 10,
+    paddingLeft: 10,
+    borderRadius: 20,
+    paddingTop: 10, // Adjust padding as needed
+  },
+  broadcastButton: {
+    height: 40,
+    width: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: 'rgba(20, 186, 280, 0.5)',
-    borderRadius: 5,
-    marginTop: 20,
+    borderRadius: 20,
   },
 });
 
 export default ChatScreen;
+
